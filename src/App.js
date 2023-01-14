2a6dc24d8297aa1631a311492d4509eabe6ef33a
Ramon Hernandez <ramongh331@gmail.com>
2023-01-13 20:06:02 -0500

    testing for deploy

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import { Outlet } from 'react-router-dom';
 import Header from './components/Header';
 import './index.css';
-import { bookmarkLoader } from './Loaders';
 
 
 function App() {
--- src/Loaders.js
+++ src/Loaders.js
@@ -5,12 +5,3 @@ export const bookmarksLoader = async () => {
     const bookmarks = await response.json();
     return bookmarks 
 }
-
-export const bookmarkLoader = async ({params}) => {
-    const response = await fetch(URL + "/bookmarks");
-    const bookmarks = await response.json()
-    const bookmark = await bookmarks.find(element => element._id === params.id)
-    
-
-
-}
